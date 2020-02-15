9696250b35699ca0235ae397c0b091b48deef284
Harlin Acero <harlina@digitalware.com.co>
2020-02-14 21:06:56 -0500

    table muestra

--- src/app/modules/billing/billing.component.ts
+++ src/app/modules/billing/billing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { ServiceBase } from 'src/app/shared/services/service.base';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { ValueTransformer } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-billing',
@@ -8,8 +9,23 @@ import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
   styleUrls: ['./billing.component.scss']
 })
 export class BillingComponent implements OnInit, AfterViewInit {
-  displayedColumns = ['id', 'name', 'progress', 'color'];
+  displayedColumns = ['id', 'name', 'reference', 'dateBill', 'dateAprobate', 'datePay', 'area', 'value', 'status', 'indicator'];
   dataSource: MatTableDataSource<UserData>;
+
+  // export interface UserData {
+  //   id: string;
+  //   name: string;
+  //   reference: string;
+  //   dateBill: Date;
+  //   dateAprobate: Date;
+  //   datePay: Date;
+  //   area: string;
+  //   value: number;
+  //   status: string;
+  //   color: string;
+  //   indicator: string;
+  // }
+
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
@@ -53,12 +69,40 @@ function createNewUser(id: number): UserData {
     NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
     NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
 
+  const area =
+    AREA[Math.round(Math.random() * (AREA.length - 1))];
+
+  const status =
+    STATUS[Math.round(Math.random() * (STATUS.length - 1))];
+  const indicadores =
+    INDICATORS[Math.round(Math.random() * (INDICATORS.length - 1))];
+
+
+
   return {
     id: id.toString(),
     name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+    reference: 'Referencia' + Math.round(Math.random() * 100),
+    area: area,
+    dateAprobate: new Date(),
+    dateBill: new Date(),
+    datePay: new Date(),
+    status: status,
+    value: Math.round(Math.random() * 1000),
+    indicator: indicadores,
+    color: getColor(indicadores)
   };
+}
+
+function getColor(indicador: string) {
+  switch (indicador) {
+    case 'A tiempo':
+      return 'green';
+    case 'Tarde':
+      return 'red';
+    case 'Pendiente Tarde':
+      return 'orange';
+  }
 }
 
 /** Constants used to fill up our data base. */
@@ -67,10 +111,20 @@ const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
 const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
   'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
   'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+const AREA = ['Compras', 'Comercial', 'Tesoreria', 'Administración'];
+const STATUS = ['Cancelada', 'Aprobada', 'Pendiente Aprobación', 'Pendiente Pago'];
+const INDICATORS = ['A tiempo', 'Tarde', 'Pendiente Tarde'];
 
 export interface UserData {
   id: string;
   name: string;
-  progress: string;
+  reference: string;
+  dateBill: Date;
+  dateAprobate: Date;
+  datePay: Date;
+  area: string;
+  value: number;
+  status: string;
   color: string;
+  indicator: string;
 }
